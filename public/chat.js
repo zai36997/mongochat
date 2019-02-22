9444b3b1544197c732d79db47df723bd41ce9037
Paramee Thongtep <zai36997@gmail.com>
2019-02-22 17:33:21 +0700

    add mongoChatromm End

---
+++ public/chat.js
@@ -0,0 +1,101 @@
+(function(){
+
+
+// Connect to socket.io
+var socket = io.connect('http://127.0.0.1:4000');
+
+
+var element = function (id) {
+    return document.getElementById(id);
+}
+var status = element('status');
+
+
+var messages = element('messages');
+var sentmessages = element('sentmessages');
+var sent = element('sent');
+var username = element('username');
+var clearBtn = element('clear');
+
+// Set default status
+var statusDefault = status.textContent;
+var setStatus = function (s) {
+    // Set status
+    status.textContent = s;
+
+    if (s !== statusDefault) {
+        var delay = setTimeout(function () {
+            setStatus(statusDefault);
+        }, 4000);
+    }
+}
+
+ // Get Status From Server
+ socket.on('status', function (data) {
+    setStatus((typeof data === 'object') ? data.message : data);
+
+    // If status is clear, clear text
+    if (data.clear) {
+        sentmessages.value = '';
+    }
+});
+
+
+// Check for connection
+if (socket !== undefined) {
+    console.log('Connected to socket...');
+    // Handle Output
+    socket.on('output', function (data) {
+        //console.log(data);
+        if (data.length) {
+            for (var x = 0; x < data.length; x++) {
+                // Build out message div
+                var message = document.createElement('div');
+                message.setAttribute('class', 'chat-message');
+                message.innerHTML = '<p><strong >' + data[x].name + '  : </strong>' + data[x].message + '</p>';
+                messages.insertBefore(message, messages.firstChild);
+                messages.appendChild(message);
+                var el = document.getElementById('messages');
+                el.scrollTop = el.scrollHeight;
+                sentmessages.value = '';
+            }
+        }
+    });
+
+   
+
+    // Handle Input
+    /*sentmessages.addEventListener('keydown', function (event) {
+        if (event.which === 13 && event.shiftKey == false) {
+            // Emit to server input
+            socket.emit('input', {
+                name: username.value,
+                message: sentmessages.value
+            });
+            event.preventDefault();
+        }
+    })
+*/
+    // Handle Chat Clear
+    clearBtn.addEventListener('click', function () {
+        socket.emit('clear'); //ส่งข้อมูลไปยัง Server
+    });
+
+    sent.addEventListener('click', function () {
+        console.log(username.value)
+        //อ่านค่า จาก input เก็บไว้ที่ตัวแปร name และ message
+        socket.emit('input', {
+            name: username.value,
+            message: sentmessages.value
+        });
+    });
+    // Clear Message
+    socket.on('cleared', function () {
+        messages.textContent = '';
+    });
+
+
+
+}
+
+})()
--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 const mongo = require('mongodb').MongoClient;
-const client = require('socket.io').listen(4000).sockets;
+const socket = require('socket.io');
+const express = require('express');
+//app setup
+const app = express();
+// start server ด้วย port 4000
+const server = app.listen(4000, function () {
+    console.log('listening to requests on prt 4000');
+});
+//Static files
+app.use(express.static('public'));
+//Socket setup
+const io = socket(server);
 
-// Connect to mongo
 mongo.connect('mongodb://127.0.0.1/mongochat', { useNewUrlParser: true }, function (err, db) {
     if (err) {
         throw err;
     }
 
-    console.log('MongoDB connected...');
 
-    client.on('connection', function (socket) {
+    io.on('connection', function (socket) {
         let myDB = db.db('mongochat');
         let chat = myDB.collection('chats');
-
+        console.log('MongoDB connected...', socket.id);
         // Create function to send status
         sendStatus = function (s) {
             socket.emit('status', s);
         }
+
         // Get chats from mongo collection
         chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
             if (err) {
                 throw err;
             }
-
             // Emit the messages
             socket.emit('output', res);
         });
+
         // Handle input events
         socket.on('input', function (data) {
             let name = data.name;
@@ -33,32 +43,27 @@ mongo.connect('mongodb://127.0.0.1/mongochat', { useNewUrlParser: true }, functi
 
             // Check for name and message
             if (name == '' || message == '') {
-                // Send error status
                 sendStatus('Please enter a name and message');
             } else {
-                // Insert message
-                chat.insert(
+                chat.insertOne(
                     { name: name, message: message }, function () {
-                        client.emit('output', [data]);
+                        io.emit('output', [data]);
 
                         // Send status object
                         sendStatus({
                             message: 'Message sent',
                             clear: true
                         });
-                    });
+                    }
+                );
             }
         });
-
         // Handle clear
         socket.on('clear', function (data) {
-            // Remove all chats from collection
-            chat.remove({}, function () {
-                // Emit cleared
+            chat.deleteMany({}, function () {
                 socket.emit('cleared');
             });
         });
-       
-
     });
 });
+
